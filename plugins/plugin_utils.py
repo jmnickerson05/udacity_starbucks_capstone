5d47cd96096e23fa2faadca6ce128d09111c684e
Jon <Jon@singlestonevdt.myfiosgateway.com>
2020-12-06 16:29:04 -0500

    Updating progress of eda

--- plugins/plugin_utils.py
+++ plugins/plugin_utils.py
@@ -42,50 +42,6 @@ def regexp_replace(txt, pattern, replacement):
 def split_part(txt, char, part_index):
 	return txt.split(char)[part_index-1]
 
-# @none_on_exception
-# @functools.lru_cache(maxsize=128)
-# def parse_sql(sql_text):
-# 	try:
-# 		return json.dumps([str(stmt) for stmt in sqlparse.split(sql_text)])
-# 	except Exception as e:
-# 		print(e)
-
-@none_on_exception
-@functools.lru_cache(maxsize=128)
-def remove_blank_lines(p_txt):
-	return '\n'.join([t for t in p_txt.split('\n') if t != ''])
-
-# @none_on_exception
-# @functools.lru_cache(maxsize=128)
-# def levenshtein(str1, str2):
-# 	return levenshtein_distance(str1, str2)
-
-@none_on_exception
-@functools.lru_cache(maxsize=128)
-def crush_txt(txt):
-	return txt.replace('\n','').replace('\t','').replace(' ','') 
-
-# @hookimpl(trylast=True)
-# def render_cell(value):
-#     if not isinstance(value, str):
-#         return None
-#     stripped = value.strip()
-#     if not (
-#         (stripped.startswith("{") and stripped.endswith("}"))
-#         or (stripped.startswith("[") and stripped.endswith("]"))
-#     ):
-#         return None
-#     try:
-#         data = json.loads(value)
-#     except ValueError:
-#         return None
-#     return jinja2.Markup(
-#         '<pre>{data}</pre>'.format(
-#             data=jinja2.escape(json.dumps(data, indent=4))
-#         )
-#     )
-
-# @hookimpl
 def prepare_connection(conn):
 	"""
 	#TO INSTANTIATE:
@@ -96,10 +52,7 @@ def prepare_connection(conn):
 	to_df = lambda x: pd.read_sql(x, conn)
 	"""
 	conn.create_function("regexp_matches", 3, regexp_matches)
-	conn.create_function('remove_blank_lines', 1, remove_blank_lines)
 	conn.create_function('regexp_replace', 3, regexp_replace)
 	conn.create_function('split_part', 3, split_part)
-	# conn.create_function('levenshtein', 2, levenshtein)
-	conn.create_function('crush_txt', 1, crush_txt)	
 	return conn
 
